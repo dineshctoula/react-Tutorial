e9eeb52374780fa9a75462f75a716f16d3947c50
dineshctoula <ds9806026041@gmail.com>
2025-11-18 17:15:17 +0545

    React program to display hell world

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import User from "./User";
+const App=()=>{
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>dinesh sitoula</p>
+      <User />
+    </div>
+  );
+};
+export default App;
---
+++ src/User.jsx
@@ -0,0 +1,11 @@
+function User()
+{
+    return (
+        <div>
+            <h1>User</h1>
+            <p> this is user</p>
+        </div>
+    );
+
+}
+export default User;
