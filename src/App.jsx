5a7166068a5792dac9a1478771091c3c35b1e458
dineshctoula <ds9806026041@gmail.com>
2025-11-24 16:21:19 +0545

    updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import User from "./User";
-const App=()=>{
+function App()
+{
+  return (
+    <div className="App">
+      <h1> welcome to React</h1>
+      <p> this is a simple react application.</p>
+      {abc()}
+      <UserOne />
+      <User />
+      
+      
+    </div>
+  )
+}
+
+
+function UserOne(){
   return (
     <div>
-      <h1>Hello world</h1>
-      <p>dinesh sitoula</p>
-      <User />
+      <h1> Users</h1>
+      <p>DINESH </p>
     </div>
-  );
-};
+  )
+}
+
+
+function abc(){
+  return (
+    <div>
+      <h1> abc</h1>
+      <p>pawan </p>
+    </div>
+  )
+}
+
+
+
+
+
 export default App;
