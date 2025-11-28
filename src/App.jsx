655a8beb2801c4eb8f898e2a1c707ef0726d5a8b
dineshctoula <ds9806026041@gmail.com>
2025-11-28 16:22:49 +0545

    Updated React import/export example

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import User from "./User";
-function App()
-{
+import User, { Profile, UserName } from "./User";
+
+function App() {
   return (
-    <div className="App">
-      <h1> welcome to React</h1>
-      <p> this is a simple react application.</p>
-      {abc()}
-      <UserOne />
+    <div>
+      <h1>Hey</h1>
       <User />
-      
-      
+      <Profile />
+     <h1>{UserName}</h1>
     </div>
-  )
+  );
 }
-
-
-function UserOne(){
-  return (
-    <div>
-      <h1> Users</h1>
-      <p>DINESH </p>
-    </div>
-  )
-}
-
-
-function abc(){
-  return (
-    <div>
-      <h1> abc</h1>
-      <p>pawan </p>
-    </div>
-  )
-}
-
-
-
-
 
 export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,11 +1,27 @@
-function User()
-{
-    return (
-        <div>
-            <h1>User</h1>
-            <p> this is user</p>
-        </div>
-    );
-
+function User() {
+  return (
+    <div>
+      <h1>User</h1>
+      <p>This is user</p>
+    </div>
+  );
 }
+
 export default User;
+
+
+
+export function Profile() {
+  return (
+    <div>
+      <h1>Profile Components</h1>
+      <p>This is profile</p>
+    </div>
+  );
+}
+
+
+
+
+
+export const UserName="dinesh";
