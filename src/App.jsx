fea3c79978647b495a34ed76b063fa6cccdbbb9a
dineshctoula <ds9806026041@gmail.com>
2025-12-01 21:20:21 +0545

    props in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { useState } from "react";
+import Hobby from "./Hobby";
 
-import Bio from "./bio";
-
-
-
-
-
-
-
-function App() {
-  const [show, setShow] = useState(false);
-
+function App(){
+  let hobbys=["reading","gaming","coding"]
   return (
-    <div>
-      <h1>Dinesh Sitoula</h1>
-
-      <button onClick={() => setShow(!show)}>
-        {show ? "Hide" : "Show"} Details
-      </button>
-
-      {/* show details conditionally */}
-      {show && (
-        <p>
-          <Bio />
-        </p>
-      )}
+    <div className="App">
+      <h1> My React App</h1>
+      <p> This is a simple react application</p>
+      <Hobby hobby={hobbys} />
     </div>
   );
 }
-
-export default App;
+export default App; 
---
+++ src/Hobby.jsx
@@ -0,0 +1,11 @@
+const Hobby=({hobby})=>{
+    return (
+        <div>
+            
+        <h3>{hobby.join(' ')}</h3>
+       
+
+        </div>
+    );
+};
+export default Hobby;
