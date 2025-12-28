adb5a3d0c5bec7f9842c6580a775cc8d848e69a5
dineshctoula <ds9806026041@gmail.com>
2025-12-28 19:28:46 +0545

    css external stylesheets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import './App.css';
 function App(){
   return (
-    <div >
-     <p style={{fontSize:"18px", fontStyle:"italic", marginBottom:"10px"}}>The only way to do great work is to love what you do.  </p>
-     <p style={{fontSize:"16px", fontStyle:"bold", color:"maroon"}}>- steve Jobs</p>
+    <div className='QuoteCard'>
+     <p className='QuoteTag'>Education is the most powerful weapon that you can use to change world. </p>
+    <p className='QuoteAuthor'> - Steve Jobs</p>
     </div>
   );
 }
 export default App;
+
