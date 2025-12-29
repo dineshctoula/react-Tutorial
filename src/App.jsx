9035496841ea9ab8da0519d1510b01f0d3a105b8
dineshctoula <ds9806026041@gmail.com>
2025-12-29 19:21:33 +0545

    29. style components in ReactJs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import style from './App.module.css';
-function App(){
-  return (
-    <div className={style.quoteCard}>
-      <p className={style.quoteTag}>Education is the most powerful that you can use to change the world</p>
-      <p className={style.quoteAuthor}>- Nelson Mandela</p>
+import { useState } from "react";
+import styled from "styled-components";
 
-    </div>
+// Container for the quote
+const Quote = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;  /* fixed typo */
+  height: 100vh;
+  background-color: maroon; /* fixed typo */
+`;
+
+// The main quote text
+const QuoteCard = styled.p`
+  font-size: 2em;
+  color: pink;
+  text-align: center;
+  margin: 0;
+  padding: 20px;
+  font-style: italic;
+`;
+
+// The author text
+const QuotesAuthor = styled.p`
+  font-size: 1.5em;
+  color: ${(props) => props.color};  // dynamic color from state
+  font-weight: bold;  /* fixed CSS property */
+  margin-top: 20px;
+`;
+
+function App() {
+  const [color, setColor] = useState("green");  // state for author color
+
+  return (
+    <Quote>
+      <QuoteCard>
+        Education is the most powerful weapon that you can use to change the world.
+      </QuoteCard>
+      <QuotesAuthor color={color}>- Nelson Mandela</QuotesAuthor>
+    </Quote>
   );
-  
 }
+
 export default App;
