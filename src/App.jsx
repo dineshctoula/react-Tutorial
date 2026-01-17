66a7280fb5338e968957ddf3d866cd7260631a65
dineshctoula <ds9806026041@gmail.com>
2026-01-17 15:30:07 +0545

    #37. UseTransition Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-export default function App() {
-  async function handleSubmit(formData) {
-    "use server";
-    await new Promise((r) => setTimeout(r, 2000));
-    console.log("Form submitted");
-  }
+import { useState, useTransition } from "react";
+
+function App() {
+  const [text, setText] = useState("Click Me");
+  const [pending, startTransition] = useTransition();
+
+  const handleClick = () => {
+    // urgent update
+    setText("Processing...");
+
+    // non-urgent update
+    startTransition(() => {
+      setTimeout(() => {
+        setText("Done!");
+      }, 2000);
+    });
+  };
 
   return (
     <div>
-      <h1>UseFormStatus Hook in React 19</h1>
-
-      <form action={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" required />
-        <br /><br />
-
-        <input type="email" name="email" placeholder="Email" required />
-        <br /><br />
-
-        <input type="password" name="password" placeholder="Password" required />
-        <br /><br />
-
-        <SubmitBtn />
-      </form>
+      <h2>useTransition Demo</h2>
+      <button disabled={pending} onClick={handleClick}>
+        {pending ? "Loading..." : text}
+      </button>
     </div>
   );
 }
+
+export default App;
