e885f365cbfcedc0d4d0c85b5920eed2c9ed607f
dineshctoula <ds9806026041@gmail.com>
2026-01-19 17:22:29 +0545

    #42. Update Object in Usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
 import { useState } from "react";
 
-function App() {
-  const [fruits, setFruits] = useState(["apple", "banana", "mango"]);
-  const [data, setData] = useState([
-    { id: 1, name: "dinesh", age: 22 },
-    { id: 2, name: "dipak", age: 23 },
-    { id: 3, name: "pawan", age: 24 },
-    { id: 4, name: "Nira", age: 25 },
-  ]);
+function App(){
+  const[user, setUser]=useState({
+    name:"Dinesh",
+    age: 22,
+    address:{
+      city:"jhapa",
+      state:"koshi",
+    },
+  });
 
-  const handleFruits = () => {
-    setFruits([...fruits, "orange"]);
-  };
 
-  const handleData = () => {
-    const newData = data.map((item) =>
-      item.id === 1
-        ? { ...item, age: item.age + 1 }
-        : item
-    );
-    setData(newData);
-  };
+  const updateAge=()=>{
+    setUser({...user, age:user.age+1});
 
-  return (
-    <div>
-      <h1>Updating array in state</h1>
+  }
+  const updateCity=()=>{
+    setUser({...user,
+      address:{
+        ...user.address,
+        city:"oslo",
+      }
+    })
+  }
+  return(
+<div>
+<h1> Update object in state</h1>
+<h2>Name:{user.name}</h2>
+<h2>Age:{user.age}</h2>
+<h2>City:{user.address.city}</h2>
+<h2>State:{user.address.state}</h2>
+<button onClick={updateAge}>Update Age</button>
 
-      <h2>My Fruits are:</h2>
-      {fruits.map((item, index) => (
-        <h3 key={index}>{item}</h3>
-      ))}
-      <button onClick={handleFruits}>Add Fruit</button>
+<button onClick={updateCity}>Update City</button>
 
-      <h2>User Data:</h2>
-      {data.map((item) => (
-        <p key={item.id}>
-          {item.name} - {item.age}
-        </p>
-      ))}
-
-      <button onClick={handleData}>Increase Age of ID 1</button>
-    </div>
+</div>
   );
 }
-
 export default App;
