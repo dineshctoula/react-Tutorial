da35a4f1d7d1439b3f9fa0a78569c1092e541907
dineshctoula <ds9806026041@gmail.com>
2026-01-19 17:57:48 +0545

    #43. React Props.children

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useState } from "react";
+import Layout from "./Layout";
+import Footer from "./Footer";
 
 function App(){
-  const[user, setUser]=useState({
-    name:"Dinesh",
-    age: 22,
-    address:{
-      city:"jhapa",
-      state:"koshi",
-    },
-  });
-
-
-  const updateAge=()=>{
-    setUser({...user, age:user.age+1});
-
-  }
-  const updateCity=()=>{
-    setUser({...user,
-      address:{
-        ...user.address,
-        city:"oslo",
-      }
-    })
-  }
   return(
 <div>
-<h1> Update object in state</h1>
-<h2>Name:{user.name}</h2>
-<h2>Age:{user.age}</h2>
-<h2>City:{user.address.city}</h2>
-<h2>State:{user.address.state}</h2>
-<button onClick={updateAge}>Update Age</button>
-
-<button onClick={updateCity}>Update City</button>
-
+  <Layout>
+    <footer>
+<p> this is parent component</p>
+    </footer>
+  </Layout>
 </div>
   );
 }
