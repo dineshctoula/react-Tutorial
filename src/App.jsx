46fe1a99aac18736b4bb0dad8b4b20c51db5c62a
dineshctoula <ds9806026041@gmail.com>
2026-01-20 17:38:40 +0545

    #44. state lifting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import Layout from "./Layout";
-import Footer from "./Footer";
+import { useState } from "react";
+import ChildA from "./ChildA";
+import ChildB from "./ChildB";
 
-function App(){
-  return(
-<div>
-  <Layout>
-    <footer>
-<p> this is parent component</p>
-    </footer>
-  </Layout>
-</div>
+function App() {
+  const [msg, setMsg] = useState("");
+
+
+  // setMsg is passed to the ChildA
+  // and msg is passed to the ChildB 
+  // state lifting bhaneko chae euta child ko data change huda arko ma ne change hunxa
+
+  return (
+    <div>
+      <h1>State lifting in React</h1>
+      <ChildA setMsg={setMsg} />
+      <ChildB msg={msg} />
+    </div>
   );
 }
+
 export default App;
---
+++ src/ChildB.jsx
@@ -0,0 +1,10 @@
+function ChildB({ msg }) {
+  return (
+    <div>
+      <h1>Child B</h1>
+      <h2>MSG: {msg}</h2>
+    </div>
+  );
+}
+
+export default ChildB;
