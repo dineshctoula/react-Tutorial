69edf4c4fcfdf398dfde33a3ad551043128458ba
dineshctoula <ds9806026041@gmail.com>
2026-01-22 17:45:35 +0545

    #47. useActionState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import Parent from "./components/Parent";
-import { UserProvider } from "./UserContext";
+import { useActionState } from "react";
 
-function App(){
-  return(
-    <div style={{backgroundColor:"green", padding:"20px"}}>
-<h1> APP component</h1>
-<UserProvider>
-  <Parent />
-</UserProvider>
+function submitFeedback(prevState, formData) {
+  const name = formData.get("name");
+  const feedback = formData.get("feedback");
 
-    </div>
+  if (name && feedback) {
+    return `Thank you ${name} for your feedback`;
+  } else {
+    return "Please enter both name and feedback";
+  }
+}
+
+export default function App() {
+  const [message, formAction, pending] = useActionState(submitFeedback, "");
+
+  return (
+    <>
+      <h1>useActionState Hook in ReactJS</h1>
+
+      <form action={formAction}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Your Name"
+        />
+        <br /><br />
+
+        <textarea
+          name="feedback"
+          placeholder="Enter your feedback"
+        />
+        <br /><br />
+
+        <button type="submit" disabled={pending}>
+          {pending ? "Submitting..." : "Submit"}
+        </button>
+
+        <p>{message}</p>
+      </form>
+    </>
   );
 }
-export default App;
